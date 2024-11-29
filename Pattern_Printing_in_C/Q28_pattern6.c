733ca038c82f7c10a9d50ac6d36d12816c2a24b4
Bijoy0369 <156492281+Bijoy0369@users.noreply.github.com>
2024-11-29 10:26:55 -1100

    first commit

---
+++ Pattern_Printing_in_C/Q28_pattern6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+ int main()
+{   
+    char str[]="C-Programming";
+    int len;
+    len=strlen(str);
+
+    for(int r=0;r<len;r++)  //rows
+    {
+        for(int c=0;c<=r;c++) //colume
+        {
+            printf("%c ",str[c]);
+            /*
+            B
+            BI
+            BIJ
+            BIJO
+            BIJOY*/
+        }    
+        printf("\n");
+        
+    }
+
+    return 0;
+}
